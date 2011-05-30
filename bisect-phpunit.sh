f4a1230cf4f881a654dd090b91a53b4990fdfde5
Toni Uebernickel <tuebernickel@gmail.com>
2011-05-30 12:07:13 +0200

    added wrapping of exit code 2 to 127

--- bisect-phpunit.sh
+++ bisect-phpunit.sh
@@ -2,7 +2,16 @@
 
 phpunit
 
-if [ "$?" -ne "0" ]; then
+EXIT_CODE="$?"
+
+if [ $EXIT_CODE -eq "255" ]; then
   # Wrapping the error code to 1, so bisect marks this build as "bad" and continues.
   exit 1
 fi
+
+if [ $EXIT_CODE -eq "2" ]; then
+  # The testsuite does not exist, so we skip bisect here.
+  exit 127
+fi
+
+exit $EXIT_CODE
